f92645be9dbc56311ac937d276c395a924fc9138
NathanFortyTwo <nathan.feret@gmail.com>
2023-10-24 00:43:18 +0200

    ajout des premieres politiques + Astar

--- src/swarm_rescue/solutions/drone_utils.py
+++ src/swarm_rescue/solutions/drone_utils.py
@@ -1,11 +1,17 @@
 import numpy as np
+from enum import Enum
+import numpy as np
+import heapq
+
+states_enum = Enum('States', ["INIT", "GOTO_TARGET", "EXPLORE"]) 
+zone_types = Enum('ZoneTypes', ["UNKNOWN", "FREE", "WALL", "RESCUE", "BASE","NO_COMM","NO_GPS","DANGER"])
+zone_cost = {zone_types.UNKNOWN.value:2,zone_types.FREE.value:1,zone_types.WALL.value:100,zone_types.RESCUE.value:1,zone_types.BASE.value:1,zone_types.NO_COMM.value:20,zone_types.NO_GPS.value:20,zone_types.DANGER.value:100}
 
 def coords_to_indexes(coords, gpsbounds,matrix_size):
     """
     Converts coordinates to indexes in the matrix
     Matrix is a square matrix of size matrix_size
     """
-
     x,y = coords
     xmin, xmax = gpsbounds
     ymin,ymax = xmin,xmax
@@ -13,3 +19,82 @@ def coords_to_indexes(coords, gpsbounds,matrix_size):
     i = (y-ymax)/(ymin-ymax)
     j = (x-xmin)/(xmax-xmin)
     return int(i*n),int(j*n)
+
+def find_on_map(matrix, value):
+    """
+    finds the coordinates of the value in the matrix
+    """
+    positions =np.where(matrix==value)
+    positions = list(zip(positions[0],positions[1]))
+    return positions
+
+
+# A* algorithm, c'est pas de  moi ça vient d'internet, 
+# je l'ai adapté pour notre cas + testé 
+
+# Define a Node class to represent a cell in the matrix
+class Node:
+    def __init__(self, x, y, cost, heuristic, parent):
+        self.x = x
+        self.y = y
+        self.cost = cost  # g(n)
+        self.heuristic = heuristic  # h(n)
+        self.total_cost = cost + heuristic  # f(n) = g(n) + h(n)
+        self.parent = parent
+
+    def __lt__(self, other):
+        return self.total_cost < other.total_cost
+
+#distance manhattan, on peut changer et prendre la distance euclidienne
+def heuristic(x1, y1, x2, y2):
+    # return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    return abs(x1 - x2) + abs(y1 - y2) 
+
+# Check if the given coordinates are valid (inside matrix and not a wall)
+def is_valid(matrix, x, y):
+    return 0 <= x < len(matrix) and 0 <= y < len(matrix[0]) and matrix[x][y] == 0
+
+def cost(matrix, x, y):
+    value = matrix[x][y]
+    return zone_cost[value]
+    
+
+def astar(matrix, start, goal):
+    moves = [(0, -1), (1, 0), (0, 1), (-1, 0),(1, 1), (-1, -1), (-1, 1), (1, -1)] 
+    # on peut enlever les diagonales si c'est trop dangereux pour les murs
+
+    start_node = Node(start[0], start[1], 0, heuristic(start[0], start[1], goal[0], goal[1]), None)
+    end_node = Node(goal[0], goal[1], 0, 0, None)
+
+    open_list = [start_node]
+    closed_list = set()
+
+    while open_list:
+        current_node = heapq.heappop(open_list)
+
+        if current_node.x == end_node.x and current_node.y == end_node.y:
+            path = []
+            while current_node:
+                path.append((current_node.x, current_node.y))
+                current_node = current_node.parent
+            return path[::-1]
+
+        closed_list.add((current_node.x, current_node.y))
+
+        for move in moves:
+            new_x, new_y = current_node.x + move[0], current_node.y + move[1]
+
+            if not is_valid(matrix, new_x, new_y) or (new_x, new_y) in closed_list:
+                continue
+
+            new_cost = current_node.cost + cost(matrix, new_x, new_y)
+            new_heuristic = heuristic(new_x, new_y, goal[0], goal[1])
+
+            existing_node = next((n for n in open_list if n.x == new_x and n.y == new_y), None)
+            if not existing_node or new_cost < existing_node.cost:
+                if existing_node:
+                    open_list.remove(existing_node)
+                new_node = Node(new_x, new_y, new_cost, new_heuristic, current_node)
+                heapq.heappush(open_list, new_node)
+
+    return None # pas de chemin
--- src/swarm_rescue/solutions/my_custom_drone.py
+++ src/swarm_rescue/solutions/my_custom_drone.py
@@ -18,12 +18,16 @@ class MyCustomDrone(DroneAbstract):
         self.counterStraight = 0
         self.angleStopTurning = random.uniform(-math.pi, math.pi)
         self.distStopStraight = random.uniform(10, 50)
+
         self.isTurning = False
         self.GPS_BOUNDS = (-500,500)
         self.MAP_SIZE = 10
         self.map = np.zeros([self.MAP_SIZE,self.MAP_SIZE])
         self.step_counter = 0
-    
+        self.states=["EXPLORE"]
+        self.grasper = 0
+        self.arrived = False
+
     def define_message_for_all(self):
         return self.map
 
@@ -35,9 +39,87 @@ class MyCustomDrone(DroneAbstract):
         i,j = du.coords_to_indexes(position,self.GPS_BOUNDS,self.MAP_SIZE)
         if self.map[i,j] == 0:
             self.map[i,j] = 1
+
+    def get_base_path(self):
+        coords = du.find_on_map(self.map,du.zone_types.BASE.value)
+        if len(coords)==0:
+            return None # c'est pas censé arriver
+         
+        position = self.measured_gps_position()
+        i,j = du.coords_to_indexes(position,self.GPS_BOUNDS,self.MAP_SIZE)
+
+         # une seule base donc on peut prendre le premier
+        base = coords[0]
+        path = du.astar(self.map,(i,j),base)
+        self.path = path
+        return self.path
+    
+    def get_closest_rescue_path(self):
+        coords = du.find_on_map(self.map,du.zone_types.RESCUE.value)
+        if len(coords)==0:
+            return None
         
+        position = self.measured_gps_position()
+        i,j = du.coords_to_indexes(position,self.GPS_BOUNDS,self.MAP_SIZE)
+        # find the path to the closest rescue
+        length = math.inf
+        for rescue in coords:
+            path = du.astar(self.map,(i,j),rescue) # on peut changer pour prendre le premier path non null
+            if path is not None:
+                if len(path) < length:
+                    self.path = path
+        return self.path
+        
+    def move_to_next_point_in_path(self):
+        # move to next point in path
+        if len(self.path) <1:
+            self.arrived= True
+            return {"forward":0,"lateral":0,"rotation":0,"grasped":self.grasper}
+        
+        position = self.measured_gps_position()
+        i,j = du.coords_to_indexes(position,self.GPS_BOUNDS,self.MAP_SIZE)
+        next_point = self.path[0]
+        if next_point[0] == i and next_point[1] == j: # si on est arrivé au point
+            self.path.pop(0) # on enlève le point
+            self.move_to_next_point_in_path() # on rappelle la fonction
+        else: # sinon on se déplace en ligne droite
+            return self.move_to(next_point[0],next_point[1])
+
+
+
+    def move_to(self, i,j):
+        # va a la prochaine case en ligne droite, normalement le chemin est sans obstacle
+        #TODO
+
+        return {"forward":0,"lateral":0,"grasper":0,"rotation":0}
+    
     def control(self):
         self.step_counter += 1
         self.update_map()
-        print(self.map if self.step_counter%100==0 else "",end="\r")
-        return {"forward": 0.7, "rotation": 0.2}
+        
+        if "GOTO_RESCUE" in self.states :
+            if self.path is None:
+                self.path = self.get_closest_rescue_path()
+            if self.path is not None:
+                if self.arrived:
+                    self.states.remove("GOTO_RESCUE")
+                    self.states.append("GOTO_BASE")
+                    self.path = None
+                else:
+                    return self.move_to_next_point_in_path() # appelé a chaque step de SAVE_RESCUE 
+                
+
+        if "GOTO_BASE" in self.states:
+            if self.path is None:
+                self.path = self.get_base_path()
+            if self.path is not None:
+                if self.arrived:
+                    self.states.remove("GOTO_BASE")
+                    self.states.append("EXPLORE")
+                    self.path = None
+                else:
+                    return self.move_to_next_point_in_path()
+            
+
+        if "EXPLORE" in self.states:
+            pass
